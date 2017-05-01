a88d6290ef9ee6fcfd1e23cec9bc1fba8a34fc40
Stav Geffen <stavgeffen@gmail.com>
2017-05-01 20:31:36 +0300

    feat(businesscard): adds autotrack to all links + logo
    also adds a 'js' gulp task for initiating autotrack

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 const gulp = require('gulp');
 const plugins = require('gulp-load-plugins')();
 
-const autoprefixer = require('autoprefixer');
+const isProduction = process.env.NODE_ENV === 'production';
+const versionHash = isProduction ? `.${ packageJson.version }.min` : '';
 
-gulp.task('prep-public', () => {
+gulp.task('assets', () => {
 
 	gulp.src('client/graphics/*')
 		.pipe(gulp.dest('public/graphics/'));
@@ -13,24 +14,41 @@ gulp.task('prep-public', () => {
 
 });
 
-gulp.task('sass', () => {
+gulp.task('css', () => {
 
-	const postcssOptions = [autoprefixer({ browsers: ['last 3 versions', '> 5%', 'ie > 8'] })];
+	const sassOptions = isProduction
+		? { outputStyle: 'compressed' }
+		: { outputStyle: 'nested', errLogToConsole: true, sourceComments : 'normal' };
 
 	return gulp.src('client/index.sass')
-		.pipe(plugins.sass({ outputStyle: 'nested', errLogToConsole: true, sourceComments : 'normal' }))
-		.pipe(plugins.postcss(postcssOptions))
-		.pipe(plugins.rename({ basename: `styles` }))
+		.pipe(plugins.sass(sassOptions))
+		.pipe(plugins.autoprefixer({ browsers: ['> 1%', 'ie > 8'] }))
+		.pipe(plugins.rename({ basename: `styles${ versionHash }` }))
 		.pipe(gulp.dest('public'));
 
 });
 
-gulp.task('pug', () => {
+gulp.task('js', () => {
+
+	return gulp.src('client/index.js')
+		.pipe(plugins.browserify())
+		.pipe(plugins.babel({ presets: ['es2015'] }))
+		.pipe(plugins.if(isProduction, plugins.uglify()))
+		.pipe(plugins.rename({ basename: `scripts${ versionHash }` }))
+		.pipe(gulp.dest('public'));
+
+});
+
+gulp.task('html', () => {
 
 	return gulp.src('client/index.pug')
-		.pipe(plugins.pug({ pretty: true }))
+		.pipe(plugins.pug({ pretty: !isProduction, verbose: !isProduction }))
+		.pipe(plugins.htmlReplace({
+			'js': `/scripts${ versionHash }.js`,
+			'css': `/styles${ versionHash }.css`
+		}))
 		.pipe(gulp.dest('public'));
 
 });
 
-gulp.task('build', plugins.sequence('prep-public', 'sass', 'pug'));
+gulp.task('build', plugins.sequence('assets', 'css', 'js', 'html'));
